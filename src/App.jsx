e5b9d3d5a3bef69252ee076b57d202db3e39c71a
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-09-19 19:04:13 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import ImageFirst from './ImageFirst';
 import iceCream from "./images/ice-cream.png"
 import strawberry from "./images/strawberry.jpg"
 import punchie1 from "./images/punchie1.jpg"
+import fanArt from "./images/fanart.jpg"
+import banana2 from "./images/banana2.jpg"
+import pPunchie from "./images/ppunchie.jpg"
+
 function App() {
  
 
@@ -27,6 +31,25 @@ function App() {
     paragraph={"The all-new Punchie Peach delivers a smooth and vibrant peach flavor. Cool down with this creamy, fruity delight and savor a taste of summer."}
     productImage={punchie1}/>
 
+  <ImageFirst
+    title={"Mango and Passion Fruit"}
+    paragraph={" Introducing the new mango and passion fruit: a tropical blend of sweet mango and tangy passion fruit. Refreshingly exotic, it's perfect for sunny days."}
+    iceCreamImage={iceCream}
+    productImage={fanArt}
+    />
+
+<TextFirst 
+    title={"Funky Banana"}
+    iceCreamImage={iceCream}
+    paragraph={"Meet Funky Banana: a creamy, playful mix of banana with a zesty twist. This smooth and refreshing treat is a must-have for any summer adventure."}
+    productImage={banana2}/>
+
+<ImageFirst
+    title={"Punchie Peach"}
+    paragraph={" Peach FanYogo The all-new Punchie Peach delivers a smooth and vibrant peach flavor. Cool down with this creamy, fruity delightand savor a taste of summer."}
+    iceCreamImage={iceCream}
+    productImage={pPunchie}
+    />
     </>
   )
 }
